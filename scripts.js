72b6d5238d6552692a4b6402b17a3c3636669c34
Anushree <anushree2002s@gmail.com>
2024-11-28 11:11:45 +0545

    working on swiper

---
+++ scripts.js
@@ -0,0 +1,25 @@
+// swiper 
+const swiper = new Swiper('.swiper-container', {
+    loop: true,
+    slidesPerView: 3,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
